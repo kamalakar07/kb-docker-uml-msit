6fe0d346dcd996cbea11a8641680d34a60dcfbbd
Kamalakar Borlakunta <locke@Borlakunta-Mac.local>
2020-11-01 12:37:13 -0600

    initial

---
+++ kbnodeapp/models/db.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+/*
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true }, (err) => {
+    if (!err) { console.log('MongoDB Connection Succeeded.') }
+    else { console.log('Error in DB connection : ' + err) }
+});
+*/
+const options = {
+    autoIndex: false, // Don't build indexes
+    reconnectTries: 30, // Retry up to 30 times
+    reconnectInterval: 500, // Reconnect every 500ms
+    poolSize: 10, // Maintain up to 10 socket connections
+    // If not connected, return errors immediately rather than waiting for reconnect
+    bufferMaxEntries: 0,
+    useNewUrlParser: true
+  }
+
+const connectWithRetry = () => {
+  console.log('MongoDB connection with retry')
+  mongoose.connect(process.env.DB_URL, options).then(()=>{
+    console.log('MongoDB is connected')
+  }).catch(err=>{
+    console.log('MongoDB connection unsuccessful, retry after 5 seconds.')
+    setTimeout(connectWithRetry, 5000)
+  })
+}
+
+connectWithRetry();
+
+require('./employee.model');
