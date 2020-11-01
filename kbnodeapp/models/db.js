36259e46f60870d5364cd2c6bc8860ac5e190e39
Kamalakar Borlakunta <locke@Borlakunta-Mac.local>
2020-11-01 13:31:12 -0600

    start script change

--- kbnodeapp/models/db.js
+++ kbnodeapp/models/db.js
@@ -6,20 +6,20 @@ mongoose.connect(process.env.DB_URL, { useNewUrlParser: true }, (err) => {
 });
 */
 const options = {
-    autoIndex: false, // Don't build indexes
-    reconnectTries: 30, // Retry up to 30 times
-    reconnectInterval: 500, // Reconnect every 500ms
-    poolSize: 10, // Maintain up to 10 socket connections
-    // If not connected, return errors immediately rather than waiting for reconnect
-    bufferMaxEntries: 0,
-    useNewUrlParser: true
-  }
+  autoIndex: false, // Don't build indexes
+  reconnectTries: 30, // Retry up to 30 times
+  reconnectInterval: 500, // Reconnect every 500ms
+  poolSize: 10, // Maintain up to 10 socket connections
+  // If not connected, return errors immediately rather than waiting for reconnect
+  bufferMaxEntries: 0,
+  useNewUrlParser: true
+}
 
 const connectWithRetry = () => {
   console.log('MongoDB connection with retry')
-  mongoose.connect(process.env.DB_URL, options).then(()=>{
+  mongoose.connect(process.env.DB_URL, options).then(() => {
     console.log('MongoDB is connected')
-  }).catch(err=>{
+  }).catch(err => {
     console.log('MongoDB connection unsuccessful, retry after 5 seconds.')
     setTimeout(connectWithRetry, 5000)
   })
